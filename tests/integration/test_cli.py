23e7116c0b842dcf9c690bef19269eb16d0eafff
Arnaud Rocher <arnaud.roche3@gmail.com>
2021-01-16 16:26:12 +0000

    Remove unneeded namespacing

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -1,11 +1,11 @@
 from .conftest import MainProcessFixture
 
 
-class TestEntrypoint:
-    def test_help_option(self, main_process: MainProcessFixture) -> None:
-        process = main_process(["--help"])
-        assert process.wait(timeout=5.0) == 0
+def test_help_option(main_process: MainProcessFixture) -> None:
+    process = main_process(["--help"])
+    assert process.wait(timeout=5.0) == 0
 
-    def test_fails_without_args(self, main_process: MainProcessFixture) -> None:
-        process = main_process([])
-        assert process.wait(timeout=5.0) == 2
+
+def test_fails_without_args(main_process: MainProcessFixture) -> None:
+    process = main_process([])
+    assert process.wait(timeout=5.0) == 2
