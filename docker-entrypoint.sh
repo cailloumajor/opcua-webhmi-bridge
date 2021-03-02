6119156bc73ee81a07d97b55e646eefa409fe6e8
Arnaud Rocher <arnaud.roche3@gmail.com>
2021-03-02 11:29:50 +0000

    Fix entrypoint modifying mounted files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 if [ "$1" = '.venv/bin/opcua-bridge' ] && [ "$(id -u)" = '0' ]; then
-    if [ -d "/certs" ]; then
+    if [ -d "/tmp/certs" ]; then
+        mkdir /certs
+        cp /tmp/certs/cert.der /tmp/certs/key.pem /certs
         chown pythonapp:pythonapp /certs/cert.der /certs/key.pem
         chmod 644 /certs/cert.der
         chmod 600 /certs/key.pem
