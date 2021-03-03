c54cb9bc7f4d0d6274482892e02f844c56b26162
Arnaud Rocher <arnaud.roche3@gmail.com>
2021-03-03 10:18:48 +0100

    Fix error at container start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = '.venv/bin/opcua-agent' ] && [ "$(id -u)" = '0' ]; then
     if [ -d "/tmp/certs" ]; then
-        mkdir /certs
+        mkdir -p /certs
         cp /tmp/certs/cert.der /tmp/certs/key.pem /certs
         chown pythonapp:pythonapp /certs/cert.der /certs/key.pem
         chmod 644 /certs/cert.der
